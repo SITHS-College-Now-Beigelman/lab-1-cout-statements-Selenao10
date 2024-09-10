4d73d11ecd60c73966ba503ee2958124944d055e
Selenao10 <selenao10@nycstudents.net>
2024-09-10 17:26:18 -0400

    Lab 1

---
+++ Ouyang_Selena_Lab_1.cpp
@@ -0,0 +1,23 @@
+//Selena Ouyang
+//In-Class Lab 1 
+//9-10-2024
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "######################################################################################" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "# Ways to access the Task Manager on your Windows computer:                          #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "#            Press the key combination Ctrl + Shift + Escape                         #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "#            Press the key combination Ctrl + Alt + Delete and select “Task Manager” #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "#            Type “Task Manager” in the Windows Start menu search                    #" << endl;
+  cout << "#                                                                                    #" << endl;
+  cout << "######################################################################################" << endl;
+   return 0;
+}
